12cc535684d60d99da3f580af0ec35e7fa15b157
L0ad1n6 <altan.mehmet.unver@gmail.com>
2022-02-01 23:11:16 -0500

    Created propper error handler

--- src/commands/errors.py
+++ src/commands/errors.py
@@ -4,76 +4,68 @@ from discord.ext.commands.errors import *
 
 class InsufficientCredit(commands.CommandError):
     pass
-
 class InsufficientRole(commands.CommandError):
     pass
-
+class UserNotConnected(commands.CommandError):
+    pass
 class NotConnected(commands.CommandError):
     pass
-
 class AlreadyConnected(commands.CommandError):
     pass
-
 class NotPlaying(commands.CommandError):
     pass
-
 class AlreadyPlaying(commands.CommandError):
     pass
-
 class EmptyQueue(commands.CommandError):
     pass
-
-def err_embed(error, author):
-    embed = discord.Embed(
-        title=f"Error Occured",
-        description=error,
-        color=0xff1919
-    )
-    embed.set_footer(
-        text=f"Command By {author}",
-        icon_url=author.avatar_url
-    )
-    return embed
 
 class Error(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
+    async def on_command_error(self, ctx: commands.Context, error):
         if isinstance(error, CommandOnCooldown):
-            embed = err_embed(
-                error=f"This command is on cooldown. Please try again after {round(error.retry_after, 1)} seconds.",
-                author=ctx.author
-            )
+            msg = f"<:yellowx:938093739283451964> Command is on **cooldown**, try again in {round(error.retry_after, 1)} seconds"
 
         elif isinstance(error, MissingPermissions):
-            embed = err_embed(
-                error=f"{ctx.author} you are missing the required permissions to run this command!",
-                author=ctx.author
-            )
+            msg = "<:yellowx:938093739283451964> Cannot run command, missing permissions"
 
         elif isinstance(error, MissingRequiredArgument):
-            embed = err_embed(
-                error=f"Missing a required argument: **{error.param}**",
-                author=ctx.author
-            )
+            msg = f"<:yellowx:938093739283451964> Missing required argument: **{error.param}**"
 
         elif isinstance(error, ConversionError):
-            embed = err_embed(
-                error=str(error),
-                author=ctx.author
-            )
+            msg = "<:yellowx:938093739283451964> Invalid argument type, could not convert"
+
+        elif isinstance(error, CommandNotFound):
+            msg = "<:yellowx:938093739283451964> The command you are trying to use does not exist"
+
+        elif isinstance(error, InsufficientCredit):
+            msg = "<:yellowx:938093739283451964> You do not have enough Social Credit to perform this action"
+
+        elif isinstance(error, InsufficientRole):
+            msg = "<:yellowx:938093739283451964> Your top role is not high enough to perform this action"
+
+        elif isinstance(error, UserNotConnected):
+            msg = "<:yellowx:938093739283451964> You are not connected to a voice channel"
+
+        elif isinstance(error, NotConnected):
+            msg = "<:yellowx:938093739283451964> I'm not connected to a voice channel"
+
+        elif isinstance(error, AlreadyConnected):
+            msg = "<:yellowx:938093739283451964> I'm already connected to a voice channel"
+
+        elif isinstance(error, NotPlaying):
+            msg = "<:yellowx:938093739283451964> Music is not playing"
+
+        elif isinstance(error, AlreadyPlaying):
+            msg = "<:yellowx:938093739283451964> Music is already playing"
+
+        elif isinstance(error, EmptyQueue):
+            msg = "<:yellowx:938093739283451964> No more songs left in queue"
 
         else:
-            dev = self.bot.get_user(586902188899696653)
-            report = discord.Embed(
-                title="Un-Documented Event Has Occured",
-            ) 
-            dev.send()
-            embed = err_embed(
-                error=f"Uh oh. Something undocumented has occured please contact <@!586902188899696653> to fix it.",
-                author=ctx.author
-            )
+            msg = "<:yellowx:938093739283451964> Something went wrong, try again or use --report"
 
-        await ctx.channel.send(embed=embed)
+        await ctx.reply(msg, mention_author=False)
+    
